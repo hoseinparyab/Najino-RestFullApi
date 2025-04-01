7dc8d8822f40fe9ee4c15a1a13b8d29e39023e65
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2025-04-02 01:39:46 +0330

    ExceptionHandler && create ursers to admin panel
    ExceptionHandler for telescope package in laravel and CRUD users part twp

--- app/Http/Controllers/Api/V1/Admin/UserController.php
+++ app/Http/Controllers/Api/V1/Admin/UserController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Dotenv\Validator;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -13,7 +16,10 @@ class UserController extends Controller
      */
     public function index()
     {
-        return 'hi';
+        $users = User::paginate();
+        return response()->json([
+            'data' => $users
+        ]);
     }
 
     /**
@@ -21,7 +27,34 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validator = \Illuminate\Support\Facades\Validator::make($request->all(),
+                [
+                    'first_name' => ['required', 'string', 'min:1', 'max:255'],
+                    'last_name' => ['required', 'string', 'min:1', 'max:255'],
+                    'email' => ['required', 'email', 'unique:users,email'],
+                    'password' => ['required', 'string', 'min:8', 'max:255'],
+                ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'errors' => $validator->errors()
+                ], 422);
+            }
+            $inputs = $validator->validated();
+            $inputs ['password'] = Hash::make($inputs['password']);
+            $user = User::create($inputs);
+
+        } catch (\throwable $th) {
+            app()[ExceptionHandler::class]->report($th);
+            return response()->json([
+                'message' => 'something is wrong try again later! ',
+            ], 500);
+        }
+
+        return response()->json([
+            'messages' => 'user created successfully',
+            'data' => $user
+        ], 200);
     }
 
     /**
@@ -29,7 +62,9 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        //
+        return response()->json([
+            'data' => $user
+        ]);
     }
 
     /**
