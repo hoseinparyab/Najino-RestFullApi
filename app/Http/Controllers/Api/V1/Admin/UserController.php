047378bbd267efa5a61df5f4f3c8d77024d65216
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2025-04-02 01:57:01 +0330

    CRUD USER-part3
    Update users

--- app/Http/Controllers/Api/V1/Admin/UserController.php
+++ app/Http/Controllers/Api/V1/Admin/UserController.php
@@ -8,6 +8,7 @@ use Dotenv\Validator;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -72,7 +73,37 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        try {
+            $validator = \Illuminate\Support\Facades\Validator::make($request->all(),
+                [
+                    'first_name' => ['required', 'string', 'min:1', 'max:255'],
+                    'last_name' => ['required', 'string', 'min:1', 'max:255'],
+                    'email' => ['required', 'email', 'unique:users,email',Rule::unique('users','email')
+                        ->ignore($user->id)],
+                    'password' => ['nullable', 'string', 'min:8', 'max:255'],
+                ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'errors' => $validator->errors()
+                ], 422);
+            }
+            $inputs = $validator->validated();
+            if (isset($inputs['password']))
+                $inputs ['password'] = Hash::make($inputs['password']);
+
+            $user->update($inputs);
+
+        } catch (\throwable $th) {
+            app()[ExceptionHandler::class]->report($th);
+            return response()->json([
+                'message' => 'something is wrong try again later! ',
+            ], 500);
+        }
+
+        return response()->json([
+            'messages' => 'user updated successfully',
+            'data' => $user
+        ], 200);
     }
 
     /**
