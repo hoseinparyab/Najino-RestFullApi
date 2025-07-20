2fb1d8a892eeffda05150f0bbf7c8c4b08c52765
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2025-07-20 14:48:58 +0330

    update Corse

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,7 +13,7 @@ class Cors
         $headers = [
             'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE, PATCH',
-            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Credentials' => 'false',
             'Access-Control-Max-Age'           => '86400',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, X-CSRF-TOKEN, X-Socket-Id, Accept, X-XSRF-TOKEN',
             'Access-Control-Expose-Headers'    => 'Authorization, X-XSRF-TOKEN',
